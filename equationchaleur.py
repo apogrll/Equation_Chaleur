bbb30a220e3a132c0030fb4a111a02d4e8abfb68
apogrll <apolline.rg@gmail.com>
2016-10-28 16:03:12 +0200

    frst

---
+++ equationchaleur.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import math
+
+
+
+def espace ():
+
+    L = 1
+    N = 100
+    d = 10
+    ls = []
+
+    
+    for x in range (0,N-1,d):
+        ls.append(x)
+        
+        
+        
+        
+        
+        for i in range (0,T-1):
+
+            
+        
+
+
+    print test(f)
+t = 1
+            
